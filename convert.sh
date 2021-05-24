b89f78ea3d0739a4d245336e596d149134564b22
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-05-24 15:34:36 +1000

    logs of updates

--- convert.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-"/Users/nick/Work/surround/rdfx/venv/bin/python" "/Users/nick/Work/surround/rdfx/convert.py" "$@"
---
+++ rdfx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"/Users/nick/Work/surround/rdfx/venv/bin/python" "/Users/nick/Work/surround/rdfx/rdfx.py" "$@"

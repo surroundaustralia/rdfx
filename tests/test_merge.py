a6a159c5e38c25f698c6632a125c6484c6627b0b
david-habgood <david.habgood@surroundaustralia.com>
2021-11-10 12:23:35 +1000

    Fix merge test

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -8,9 +8,10 @@ def test_merge_directory():
     output_format = "ttl"
     input_dir = Path(Path(__file__).parent / "data")
     files = [file for file in input_dir.glob("*.*")]
-    output_file = input_dir / f"merged.{output_format}"
+    output_filename = "output_filename"
+    output_file = input_dir / f"output_filename.{output_format}"
     ps = File(input_dir)
-    merge(files, ps, output_format)
+    merge(files, ps, output_format, output_filename)
     assert output_file.exists()
     # delete the file
     output_file.unlink()
